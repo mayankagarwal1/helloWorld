c9648df2627112bd64deabf211de7e886a0d6f7c
Mayank Agarwal <55251104+mayankagarwal1@users.noreply.github.com>
2023-10-05 11:53:32 +0530

    Update HelloWorld.java
    changes the hello world statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
   {
             public static void main(String args[])
             {
-                System.out.println("Hello World. Welcome to Wrold of Jenkins.");
+                System.out.println("Hello World. Welcome to Wrold of Jenkins. I changed this code");
             }
   }
   
